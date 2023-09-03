f1e2d643fd26c9b8c308b8b937551ac62e54304a
Artur <100250867+arturfurtado@users.noreply.github.com>
2023-09-03 02:13:24 -0300

    Division verifier

--- calculadora.c
+++ calculadora.c
@@ -45,26 +45,26 @@ int main()
         break;
 
     case 4:
-        printf("Digite o primeiro valor: ");
-        scanf("%d", &numero1);
-
-        printf("Digite o segundo valor:");
-        scanf("%d", &numero2);
-
-        if (numero2 != 0)
+        do
         {
-            resultado = numero1 / numero2;
-            printf("Resultado: %d\n", resultado);
-        }
-        else
-        {
-            printf("Divisão por 0 não é permitida");
-        }
+            printf("Digite o primeiro valor: ");
+            scanf("%d", &numero1);
+
+            printf("Digite o segundo valor:");
+            scanf("%d", &numero2);
+
+            if (numero2 == 0)
+            {
+                printf("Divisão por 0 não é permitida");
+            }
+        } while (numero2 == 0);
+        resultado = numero1 / numero2;
+        printf("Resultado: %d\n", resultado);
         break;
 
     default:
         printf("Opção não reconhecida, selecione 1, 2, 3 ou 4.\n");
     }
 
-            return 0;
-    }
+    return 0;
+}
