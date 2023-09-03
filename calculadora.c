204c620cfde5a20872ab7d5cb62865d4f337becd
Artur <100250867+arturfurtado@users.noreply.github.com>
2023-09-03 01:42:42 -0300

    Add files via upload

--- calculadora.c
+++ calculadora.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
-int main() {
-    printf("Olá, mundo!\n");
+int numero1;
+int numero2;
+int resultado;
+
+int main()
+{
+    int escolha;
+    printf("Qual operação você quer realizar? 1:Soma, 2:Substração\n");
+    scanf("%d", &escolha);
+
+    if (escolha == 1)
+    {
+        printf("Digite o primeiro valor: ");
+        scanf("%d", &numero1);
+
+        printf("Digite o segundo valor: ");
+        scanf("%d", &numero2);
+
+        resultado = numero1 + numero2;
+        printf("Resultado: %d\n", resultado);
+    }
+    else if (escolha == 2)
+    {
+        printf("Digite o primeiro valor: ");
+        scanf("%d", &numero1);
+
+        printf("Digite o segundo valor: ");
+        scanf("%d", &numero2);
+
+        resultado = numero1 - numero2;
+        printf("Resultado: %d\n", resultado);
+    }
+    else
+    {
+        printf("Escolha inválida\n");
+    }
+
     return 0;
 }
